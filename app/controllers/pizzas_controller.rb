9380ccd6c67e72c9ee2cd4df6a5f2b4b439ae526
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-07-02 09:52:10 +0300

    completeed controllers

---
+++ app/controllers/pizzas_controller.rb
@@ -0,0 +1,10 @@
+
+class PizzasController < ApplicationController
+
+    # GET /pizzas
+    def index
+        pizzas = Pizza.all
+        render json: pizzas, status: :ok
+    end
+
+end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,54 +1,31 @@
 class RestaurantsController < ApplicationController
-    def index
-      restaurants = Restaurant.all
-      render json: restaurants, only: [:id, :name, :address]
-    end
-  
-    def show
-      restaurant = Restaurant.includes(:pizzas).find_by(id: params[:id])
-  
-      if restaurant
-        render json: restaurant.as_json(include: { pizzas: { only: [:id, :name, :ingredients] } })
-      else
-        render json: { error: 'Restaurant not found' }, status: :not_found
-      end
-    end
-  
-    def destroy
-      restaurant = Restaurant.find_by(id: params[:id])
-  
-      if restaurant
-        restaurant.restaurant_pizzas.destroy_all
-        restaurant.destroy
-        head :no_content
-      else
-        render json: { error: 'Restaurant not found' }, status: :not_found
-      end
-    end
+  rescue_from ActiveRecord::RecordNotFound, with: :restaurant_not_found
+  # GET /restaurants
+  def index
+   restaurants = Restaurant.all
+   render json: restaurants, status: :ok
   end
-  
-  class PizzasController < ApplicationController
-    def index
-      pizzas = Pizza.all
-      render json: pizzas, only: [:id, :name, :ingredients]
-    end
+
+  # GET /restaurants/:id
+  def show
+      restaurant = Restaurant.find(params[:id])
+      render json: restaurant, serializer: RestaurantwithpizzaSerializer, status: :ok
   end
-  
-  class RestaurantPizzasController < ApplicationController
-    def create
-      restaurant_pizza = RestaurantPizza.new(restaurant_pizza_params)
-  
-      if restaurant_pizza.save
-        render json: restaurant_pizza.pizza.as_json(only: [:id, :name, :ingredients])
-      else
-        render json: { errors: restaurant_pizza.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def restaurant_pizza_params
-      params.require(:restaurant_pizza).permit(:price, :restaurant_id, :pizza_id)
-    end
+
+  # DELETE /restaurants/:id
+  def destroy
+      restaurant = Restaurant.find(params[:id])
+      restaurant.destroy
+      render json: {}, status: :no_content
   end
-  
+
+
+  private
+
+  def restaurant_not_found
+      render json: {error: "Restaurant not found"}, status: :not_found
+  end
+
+
+
+end
